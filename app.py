91b8032a231f30ffb41626cf2e7c605db8b5e2c1
a-842 <131918994+a-842@users.noreply.github.com>
2025-02-13 21:11:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "This is Web1 (Main Site)"
+    return render_template('index.html')
+
+@app.route('/references')
+def references():
+    return render_template('references.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/rants')
+def rants():
+    return render_template('rants.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/links')
+def links():
+    return render_template('links.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
